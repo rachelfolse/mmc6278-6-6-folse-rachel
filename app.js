7e4b8bda9913023ff5402db0a8191b3deb3eaa6f
Rachel Folse <rachelfolse3@gmail.com>
2024-10-02 16:13:53 -0400

    update

--- app.js
+++ app.js
@@ -3,11 +3,14 @@ const apiRoutes = require('./routes/api-routes')
 const htmlRoutes = require('./routes/html-routes')
 const app = express()
 // TODO: Require Exphbs
+const exphbs = require('express-handlebars')
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
 // TODO: Add the handlebars setup code
+app.engine('handlebars', exphbs.engine())
+app.set('view engine', 'handlebars')
 
 app.use(express.static('public'))
 
