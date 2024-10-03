19b51e0c48c713f4d5203b120e38b598695fb229
Rachel Folse <rachelfolse3@gmail.com>
2024-10-03 15:52:16 -0400

    heroku problem

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 const express = require('express')
 const apiRoutes = require('./routes/api-routes')
 const htmlRoutes = require('./routes/html-routes')
+import sslRedirect from 'heroku-ssl-redirect';
+import express from 'express';
 const app = express()
+
+app.use(sslRedirect());
+ 
+app.get('/', (req, res) => {
+  res.send('hello world');
+});
+ 
+app.listen(process.env.PORT || 3000);
+
+app.use(sslRedirect(['production'], 301));
 // TODO: Require Exphbs
 const exphbs = require('express-handlebars')
 
