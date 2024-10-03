bfcdb78ba6d7d26e113036423f35c008b64886e9
Rachel Folse <rachelfolse3@gmail.com>
2024-10-03 16:03:25 -0400

    ...

--- app.js
+++ app.js
@@ -1,21 +1,12 @@
 const express = require('express')
 const apiRoutes = require('./routes/api-routes')
 const htmlRoutes = require('./routes/html-routes')
-import sslRedirect from 'heroku-ssl-redirect';
 import express from 'express';
 const app = express()
-
-app.use(sslRedirect());
  
 app.get('/', (req, res) => {
   res.send('hello world');
 });
- 
-app.listen(process.env.PORT || 3000, function(){
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-  });
-
-app.use(sslRedirect(['production'], 301));
 // TODO: Require Exphbs
 const exphbs = require('express-handlebars')
 
